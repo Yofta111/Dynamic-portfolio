2cd2c5d73d715468b5ca2777e8340abad50f1f21
Yofta111 <yoftaraya@gmail.com>
2025-11-28 21:17:31 +0300

    layout fixed,with js error, asset connected, new layout added

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -28,7 +28,7 @@
 
         <div class="collapse navbar-collapse justify-content-md-center" id="navbarsExample09">
             <ul class="navbar-nav">
-                <li class="nav-item"><a class="nav-link" href="#section-home">Home</a></li>
+                <li class="nav-item"><a class="nav-link" href="#section-home">Dashboard</a></li>
                 <li class="nav-item"><a class="nav-link" href="#section-portfolio">Portfolio</a></li>
                 <li class="nav-item"><a class="nav-link" href="#section-resume">My Education</a></li>
                 <li class="nav-item"><a class="nav-link" href="#section-about">About</a></li>
@@ -388,12 +388,6 @@
                     <li>
                         <span class="text-uppercase">Phone</span>
                         +251 915771277
-                    </li>
-                    <!--						<li>-->
-                    <!--							<span class="text-uppercase">Fax</span>-->
-                    <!--							+251 915771277-->
-                    <!--						</li>-->
-                    <li>
                         <span class="text-uppercase">Address</span>
                         Addis Ababa, ET <br>
                         Kolfe Keranio  <br>
